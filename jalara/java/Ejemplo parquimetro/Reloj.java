062bbbb4af7556d876dfdce860f3109346c5cd06
jalara <jalara@5f5d3a10-4f3c-468a-a4ff-0f32ee1b3fc7>
2015-03-06 05:43:29 +0000

    Se agrego la clase de Parquimetro y boleto, en la clase reloj se añadieron mas metodos y se solucionaron errores

--- jalara/java/Ejemplo parquimetro/Reloj.java
+++ jalara/java/Ejemplo parquimetro/Reloj.java
@@ -1,18 +1,34 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Reloj
 {
     private Display horas;
     private Display minutos;
+    private int formato;
 
     public Reloj()
     {
         horas = new Display();
         minutos = new Display();
+        horas.setLimite(24);
+        minutos.setLimite(60);
+        formato = 0;
     }
     
     public String toString(){
         String cadena;
+        String fa = " AM";
         
-        cadena = horas+":"+minutos;
+        if(formato == 0){
+            cadena = horas+":"+minutos;
+        }
+        else{
+           if(horas.getValor() >= 12){
+               fa = " PM";
+            }
+            cadena = convierteHoras()+":"+minutos+fa+".";
+        }
         return(cadena);
     }
     
@@ -23,5 +39,37 @@ public class Reloj
     
     public void incMin() {
         minutos.incValor();
+        if(minutos.getValor() == 0){
+            horas.incValor();
+        }
     }
+    
+    public void cambiaFormato(){
+        if(formato == 0){
+            formato = 1;
+        }
+        else{
+            formato = 0;
+        }
+    }
+    
+    private String convierteHoras(){
+        String cadena;
+        int aux;
+        
+        aux = horas.getValor();
+        if(horas.getValor() > 12){
+            aux = horas.getValor() - 12;
+        }
+        cadena = aux + "";
+        
+        return(cadena);
+    }
+    
+    public void ponerHoraActual(){
+        Calendar c = new GregorianCalendar();
+        
+        this.setHoraMinuto(c.get(Calendar.HOUR_OF_DAY),c.get(Calendar.MINUTE));
+    }
+            
 }
