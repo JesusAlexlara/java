f82e6f0a0e515a37e09e3a4c8b62a702d5aa51d9
jalara <jalara@5f5d3a10-4f3c-468a-a4ff-0f32ee1b3fc7>
2015-03-10 17:48:28 +0000

    Initial sharing of project

---
+++ Ejercicio Empleados/Asalariado.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Asalariado here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Asalariado extends Empleado
+{
+    private float salario;
+    
+    public Asalariado(float unSalario){
+        salario = unSalario;
+    }
+    
+    public void calculaSalario(){
+        sueldo = salario;
+    }
+}
