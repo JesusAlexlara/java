73744353026d20e15212fefb9b658b807f268342
jalara <jalara@5f5d3a10-4f3c-468a-a4ff-0f32ee1b3fc7>
2015-03-13 04:43:36 +0000

    Se agregaron las clases de mamifero y ave

---
+++ jalara/java/Ejercicio Reino animal/Mamifero.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Mamifero here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Mamifero extends Animal
+{
+    public Mamifero()
+    {
+    }
+}
